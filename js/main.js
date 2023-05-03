6c1557faeac60fd10b42f69773c915c9db2f5335
Antonio <antonioboksic1603@gmail.com>
2023-05-03 15:28:02 +0200

    impostato tutto l'esercizio, mi manca da correggere logica parte "restituisci messaggio accesso"

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
 // EMAIL
 
 // chiedere all utente la sua mail
+let userEmail = prompt(",inserisci la tua mail");
+console.log(userEmail)
+// inventati una lista di chi può accedere
+const whoCanAccess = ["simone@gmail.com", "massimo@gmail.com", "tony@gmail.com"];
+console.log(whoCanAccess);
 // controlla che sia nella lista di chi può accedere
+for (i=0 ; i<whoCanAccess.length ; i++) {
+    
+    if (userEmail == whoCanAccess[i]) {
+        accessText = "Accesso effettuato"
+    } else {
+        accessText = "Accesso negato"
+    }
+    console.log(accessText)
+};
 // stampa messaggio esito controllo
+
+
 
 // DADI
 
@@ -12,7 +28,8 @@ console.log(numeroGiocatore);
 // generare numero random da 1 a 6 per computer
 let numeroComputer = Math.floor(Math.random() * 6) +1;
 console.log(numeroComputer);
-// stabilire vincitore
+
+// stabilire vincitore e restituire messaggio con esito
 
 if (numeroGiocatore > numeroComputer)
 {
