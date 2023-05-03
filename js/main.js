63df5228f9f2dc9c60a0fa0cfc9d6e585966315b
Antonio <antonioboksic1603@gmail.com>
2023-05-03 14:40:36 +0200

    partito da logica dadi, impostato prima parte (genera un numero per giocatore e un numero per computer)

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// EMAIL
+
+// chiedere all utente la sua mail
+// controlla che sia nella lista di chi può accedere
+// stampa messaggio esito controllo
+
+// DADI
+
+// generare numero random da 1 a 6 per giocatore
+let numeroGiocatore = Math.floor(Math.random() * 6) +1;
+console.log(numeroGiocatore);
+// generare numero random da 1 a 6 per computer
+let numeroComputer = Math.floor(Math.random() * 6) +1;
+console.log(numeroComputer);
